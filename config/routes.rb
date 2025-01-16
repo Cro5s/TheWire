42dca27d9958ae36a64fda16fc5aae3912d16885
Kadeem Jackson <kdjackson35@gmail.com>
2025-01-15 20:32:16 -0500

    Use resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/blogs/new", to: "blogs#new", as: :new_blog
-  get "/blogs/:id", to: "blogs#show", as: :blog
-  patch "/blogs/:id", to: "blogs#update"
-  delete "/blogs/:id", to: "blogs#destroy"
-  get "/blogs/:id/edit", to: "blogs#edit", as: :edit_blog
-  post "/blogs", to: "blogs#create", as: :blogs
+  resources :blogs
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
